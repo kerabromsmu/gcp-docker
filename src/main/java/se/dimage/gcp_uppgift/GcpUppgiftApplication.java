2c96ebf65120bc3165b6973e67393011c04a183a
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-12-29 11:22:04 +0100

    Initial commit

---
+++ src/main/java/se/dimage/gcp_uppgift/GcpUppgiftApplication.java
@@ -0,0 +1,13 @@
+package se.dimage.gcp_uppgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpUppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpUppgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/dimage/gcp_uppgift/GcpUppgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package se.dimage.gcp_uppgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpUppgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
